4ebc0fe043d68650e270a74845c2e7ab24fec312
Genevie Giko <gikogenevie@yahoo.com>
2022-05-14 20:07:11 -0400

    update codes

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -19,20 +19,24 @@ module.exports = {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
+    if (!token) {
+      return req;
+    }
+
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({ email, name, _id }) {
+    const payload = { email,  name, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
